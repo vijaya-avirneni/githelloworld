e2e147fdd66753731aa3db7e247188ca1a44272a
vijaya-avirneni <vijayaavirneni9393@gmail.com>
2021-02-18 00:39:09 +0100

    commit 2 files

--- src/githelloworld/test1.java
+++ src/githelloworld/test1.java
@@ -3,7 +3,14 @@ package githelloworld;
 public class test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String a = " Sai Ram ";
+		if(a =="Sai Ram ")
+			System.out.println("Yes");
+		else 
+		{
+			System.out.println("No");
+			
+		}
 
 	}
 
---
+++ src/githelloworld/test2.java
@@ -0,0 +1,14 @@
+package githelloworld;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		double b = 10.5d;
+		float c = 11.1f;
+		System.out.println("final value = " + (a*b-c ));
+		
+
+	}
+
+}
