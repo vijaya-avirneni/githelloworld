ed71db52233ffa8abf72d51cdf84f00f940a8463
vijaya-avirneni <vijayaavirneni9393@gmail.com>
2021-02-18 01:35:19 +0100

    while is successful

--- src/githelloworld/test2.java
+++ src/githelloworld/test2.java
@@ -5,10 +5,14 @@ public class test2 {
 	public static void main(String[] args) {
 		int a = 10;
 		double b = 10.5d;
-		float c = 11.1f;
-		System.out.println("final value = " + (a*b-c ));
+				a = 10;
+				while (a-- > 1);
+				System.out.println(a);
+				
+				
+				
+				
 		
-
 	}
 
 }
